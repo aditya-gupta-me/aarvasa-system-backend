73ba8848ef6519f2886f2571080e885c58b07533
Mohammed-Azhan <azhanpalli@gmail.com>
2025-07-05 21:05:11 +0530

    final

--- auth_service/controllers/authController.js
+++ auth_service/controllers/authController.js
@@ -89,7 +89,7 @@ exports.login = async (req, res) => {
   }
 
   const accessToken = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-    expiresIn: "1m",
+    expiresIn: "15m",
   });
 
   const refreshToken = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
